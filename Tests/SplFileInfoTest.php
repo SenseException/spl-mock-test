a4ee3b262d5d628308ae908b181af31297d532e5
Claudio Zizza <claudio@budgegeria.de>
2015-06-12 23:18:42 +0200

    Add SplFileInfo test

---
+++ Tests/SplFileInfoTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests;
+
+class SplTest extends PHPUnit_Framework_TestCase
+{
+    public function testSplError()
+    {
+        // the following mock creation triggers an error
+
+        $spl = $this->getMockBuilder(SPLFileInfo::CLASS)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // this test doesn't work
+        $spl->expects($this->once())
+            ->method('getFileName')
+            ->will($this->returnValue('Foo.php'));
+
+        $this->assertEquals('Foo.php', $spl->getFileName());
+    }
+
+    public function testSplWorks()
+    {
+        // Constructor gets a parameter for this mock
+        $spl = $this->getMockBuilder(SPLFileInfo::CLASS)
+            ->setConstructorArgs([__DIR__])
+            ->getMock();
+
+        $spl->expects($this->once())
+            ->method('getFileName')
+            ->will($this->returnValue('Foo.php'));
+
+        $this->assertEquals('Foo.php', $spl->getFileName());
+    }
+}
